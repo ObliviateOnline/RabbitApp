f80ce51781dbbce02e513aa8f3247bf49c3ccc39
zhengrf <36234958+ObliviateOnline@users.noreply.github.com>
2023-01-10 17:59:57 +0800

    1
    启用viewBinding和DataBinding

--- app/src/main/java/com/example/rabbitapp/MainActivity.kt
+++ app/src/main/java/com/example/rabbitapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.rabbitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.rabbitapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        window.attributes.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN
     }
 }
